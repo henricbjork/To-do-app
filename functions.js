a451e20a39034b3dc2d90d0d2138fc468a00fa7a
Henric <henric.bjorkvall@gmail.com>
2020-02-20 18:55:04 +0100

    First commit

---
+++ functions.js
@@ -0,0 +1,21 @@
+/**
+ * Adds functionality to the delete buttons.
+ * Selects all deletebuttons and gets parent div for each button
+ * Then gets the index of the card item in each div
+ * then removes the div and removes the item from the chores array based on the fetched index
+ * then sets the new modified array to the local storage.
+ *
+ */
+function deleteButtonHandler() {
+  const deleteButtons = document.querySelectorAll('.btn-delete');
+
+  deleteButtons.forEach(deleteButton => {
+    deleteButton.addEventListener('click', e => {
+      const card = deleteButton.closest('.list__card');
+      const cardItemIndex = chores.indexOf(card.childNodes[0].innerHTML);
+      card.remove();
+      chores.splice(cardItemIndex, 1);
+      localStorage.setItem('listItem', JSON.stringify(chores));
+    });
+  });
+}
---
+++ main.js
@@ -0,0 +1,54 @@
+'use strict';
+const template = `<div class="header">
+    <h1 class="header__text">To do list</h1>
+</div>
+    <form class="input_form">
+    <label for="input_form__input">Input todo</label>
+        <input type="text "name="input_form__input" id="input_form__input" class="input_form__input"  required>
+        <button type="submit" class="btn input_form__button">submit</button>
+    </form>
+    <p class="user_prompt"></p>
+    <ul class="list">
+    </ul>
+`;
+document.body.innerHTML = template;
+
+const submitButton = document.querySelector('.input_form__button');
+const userInput = document.querySelector('.input_form__input');
+const userPrompt = document.querySelector('.user_prompt');
+const list = document.querySelector('.list');
+const inputForm = document.querySelector('.input_form');
+const listCards = document.querySelectorAll('.list__card');
+const items = JSON.parse(localStorage.getItem('listItem'));
+
+let chores = [];
+
+if (items === null || Object.entries(items).length === 0) {
+  userPrompt.innerHTML = 'Add your first chore!';
+} else {
+  localStorage.setItem('listItem', JSON.stringify(items));
+  chores = JSON.parse(localStorage.getItem('listItem'));
+  items.forEach(item => {
+    list.innerHTML += `<div class="list__card"><li class="card__item">${item}</li><button class="btn btn-delete">Delete</button></div>`;
+  });
+
+  // Add functionality to the generated delete buttons (functions.js)
+  deleteButtonHandler();
+}
+
+inputForm.addEventListener('submit', function(event) {
+  if (userInput.value.trim() === '') {
+    window.alert('Gotta write something my man');
+  } else {
+    event.preventDefault();
+    chores.push(userInput.value);
+    userInput.value = '';
+    userPrompt.innerHTML = '';
+    localStorage.setItem('listItem', JSON.stringify(chores));
+    const items = JSON.parse(localStorage.getItem('listItem'));
+    list.innerHTML += `<div class="list__card"><li class="card__item">${items.pop()}</li><button class="btn btn-delete">Delete</button></div>`;
+
+    // Add functionality to the generated delete buttons (functions.js)
+    deleteButtonHandler();
+  }
+});
